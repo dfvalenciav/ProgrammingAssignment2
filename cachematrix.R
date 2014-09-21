467a684d7409139f2af2c6233cbdb738b2e4ff6d
Daniel <dfvalenciav@gmail.com>
2014-09-21 14:19:42 -0500

    My R for assigment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First the function takes a set of numbers (c). Next, it calculates the length
+## of c, next, it takes the half of length (b) which is the number of nrow and
+## ncol. Next it takes the this numbers and made a square matrix. Finally,
+## it calculates the inverse matrix 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(c) {
+    a<-length(c)
+    b<-a/2
+    d<-round(b, 0)
+    x<-matrix(c,nrow=d,ncol=d)
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+}
 }
 
 
-## Write a short comment describing this function
+## this function checks if the inverse of matrix have been calculated above. If 
+## it have not been calculated, this function do it and finally it returns the 
+## inverse of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m   
+    ## Return a matrix that is the inverse of 'x'
 }
